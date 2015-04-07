fa7812f20526ad44c45a5ffc9d23e3562ff98826
novicaa <novicamiketic@gmail.com>
2015-04-07 14:19:17 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,18 @@
+
+<?php
+$adresa[0]="1190 3rd Street South Naples, Florida 34102 USA"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[0]="Florida 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[0]="Naples";
+$telephone[0]=12396498088;// Broj koji se poziva na dugme CALL US
+
+$adresa[1]="The Glebe House Bletchingley Road Nutfield, Surrey, UK.RH1 4HP"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+$nazivlokacije[1]="London";
+$telephone[1]=441737821427;// Broj koji se poziva na dugme CALL US
+
+$companynumber=2;
+$theme="b";//Thema koja se aplicira na sve
+$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
